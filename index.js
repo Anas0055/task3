425bd65326c5aea8c10f2c08440b26d94f742ab5
Anas0055 <manaskhan969@gmail.com>
2024-01-12 22:12:29 -0800

    task3

---
+++ index.js
@@ -0,0 +1,3 @@
+var quote = "A person who never made a mistake never tried anything new.";
+var outhor = "Albert Einstein";
+console.log("".concat(outhor, "once said,\"").concat(quote, "\""));
---
+++ index.ts
@@ -0,0 +1,3 @@
+let quote:string = "A person who never made a mistake never tried anything new.";
+let outhor:string = "Albert Einstein";
+console.log (`${outhor}once said,"${quote}"`);
